02de3016dcc6ca96ee647ee3d2ccdf4ac5cc540b
Mikalai <45792926+kolyamoloko@users.noreply.github.com>
2023-01-17 17:03:44 +0400

    create: 1 project
    1 project created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './index.scss';
+import { useState } from 'react';
 
 function App() {
+  const [seconds, setSeconds] = useState(0);
+  const minus = () => {
+    setSeconds(seconds - 1)
+  }
+  const plus = () => {
+    setSeconds(seconds + 1);
+  }
+
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{seconds}</h1>
+        <button 
+          className="minus" 
+          onClick={minus}
+        >- Минус
+        </button>
+        <button 
+        className="plus"
+        onClick={plus}
+        >Плюс +
+        </button>
       </div>
     </div>
   );
